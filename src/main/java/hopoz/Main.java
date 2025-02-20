9024e0ace2c0d9fa15393ec162a05f619c8ab21a
entelechy <98585204+HopoZ@users.noreply.github.com>
2025-02-20 05:54:40 +0000

    add

---
+++ src/main/java/hopoz/Main.java
@@ -0,0 +1,10 @@
+package hopoz;
+
+import hopoz.class_test.myClass;
+
+public class Main {
+    public static void main(String[] args) {
+        Class <?> clazz = myClass.class;
+        System.out.println(clazz);
+    }
+}
